9a55958799270abe464d634df33641f970e616fb
Aminat Yakubu <yakubuaminat94@gmail.com>
2024-02-10 01:08:48 +0100

    Create blocking.js

---
+++ blocking.js
@@ -0,0 +1,10 @@
+var script = document.createElement('script');
+    
+    // Set the script source to your JavaScript file
+    script.src = 'https://assets-global.website-files.com/657c505868aea6c85dcb657c/js/webflow.3b63b9d27.js';
+
+    // Make the script load asynchronously
+    script.async = true;
+
+    // Append the script to the document head
+    document.head.appendChild(script);
